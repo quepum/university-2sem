73ad2dbb9f27efa3599534bde408b04e54676a13
quepum <118292850+quepum@users.noreply.github.com>
2025-02-17 23:59:17 +0300

    feat: Reading an incoming line

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    public static int Main(string[] args)
+    {
+        string? inputString = Console.ReadLine();
+        if (inputString is null)
+        {
+            Console.WriteLine("Всё плохо");
+            return -1;
+        }
+
+        return 1;
+    }
+}
