f6cd1b91a47d6d18c66486d0dc10b1d54a858fb4
quepum <118292850+quepum@users.noreply.github.com>
2025-02-20 20:44:05 +0300

    test: Add tests

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -1,5 +1,29 @@
 ﻿internal class Program
 {
+    public static int Main(string[] args)
+    {
+        if (!RunAllTests())
+        {
+            Console.WriteLine("Tests failed");
+            return -1;
+        }
+
+        string? inputString = Console.ReadLine();
+        if (inputString is null)
+        {
+            Console.WriteLine("Input error");
+            return -1;
+        }
+
+        Console.WriteLine($"Input string: {inputString}");
+        (string output, int position) = Bwt(inputString);
+        Console.WriteLine($"After BWT: {output}\nPosition: {position}");
+        string originalString = ReverseBwt(output, position);
+        Console.WriteLine($"Recovered string: {originalString}");
+
+        return 0;
+    }
+
     private static (string, int) Bwt(string input)
     {
         input += "$";
@@ -47,9 +71,9 @@
             }
         }
 
-        char[] result = new char[length];
+        char[] result = new char[length - 1];
         int currentPosition = position;
-        for (int i = 0; i < length; i++)
+        for (int i = 0; i < length - 1; i++)
         {
             result[i] = firstColumn[currentPosition];
             currentPosition = nextIndex[currentPosition];
@@ -58,21 +82,29 @@
         return new string(result);
     }
 
-    public static int Main(string[] args)
+    private static bool RunAllTests()
     {
-        string? inputString = Console.ReadLine();
-        if (inputString is null)
+        string inputTest1 = "a";
+        (string outputTest1, int testPosition1) = Bwt(inputTest1);
+        if (inputTest1 != ReverseBwt(outputTest1, testPosition1))
         {
-            Console.WriteLine("Input error");
-            return -1;
+            return false;
         }
 
-        Console.WriteLine($"Input string: {inputString}");
-        (string output, int position) = Bwt(inputString);
-        Console.WriteLine($"After BWT: {output}\nPosition: {position}");
-        string originalString = ReverseBwt(output, position);
-        Console.WriteLine($"Recovered string: {originalString}");
+        string inputTest2 = "banana";
+        (string outputTest2, int testPosition2) = Bwt(inputTest2);
+        if (inputTest2 != ReverseBwt(outputTest2, testPosition2))
+        {
+            return false;
+        }
 
-        return 0;
+        string inputTest3 = "mississippi";
+        (string outputTest3, int testPosition3) = Bwt(inputTest3);
+        if (inputTest3 != ReverseBwt(outputTest3, testPosition3))
+        {
+            return false;
+        }
+
+        return true;
     }
 }
