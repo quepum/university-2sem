9a88470bb4228e4916d2b4ac8d1c32f2f9cf6299
quepum <118292850+quepum@users.noreply.github.com>
2025-02-22 17:15:08 +0300

    refactor: Add comments and update BWT function

--- Homework1/Homework1/Program.cs
+++ Homework1/Homework1/Program.cs
@@ -1,6 +1,15 @@
-﻿internal class Program
+﻿namespace Homework1;
+
+/// <summary>
+/// Burrows Wheeler algorithm.
+/// </summary>
+internal static class Program
 {
-    public static int Main(string[] args)
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    /// <returns>The result of the program execution.</returns>
+    public static int Main()
     {
         if (!RunAllTests())
         {
@@ -24,43 +33,76 @@
         return 0;
     }
 
-    private static (string, int) Bwt(string input)
+    /// <summary>
+    /// Burrows-Wheeler transformer.
+    /// </summary>
+    /// <param name="input">Transformable string.</param>
+    /// <returns>Transformed string and position of the end.</returns>
+    private static (string OutputString, int Position) Bwt(string input)
     {
-        input += "$";
         int inputLength = input.Length;
-        string[] rotations = new string[inputLength];
+        var rotations = new int[inputLength];
 
-        for (int i = 0; i < inputLength; i++)
+        for (var i = 0; i < inputLength; i++)
         {
-            string rotation = input.Substring(i) + input.Substring(0, i);
-            rotations[i] = rotation;
+            rotations[i] = i;
         }
 
-        Array.Sort(rotations);
-        int position = Array.IndexOf(rotations, input);
-        char[] lastCharacters = new char[inputLength];
+        Array.Sort(rotations, (a, b) => CompareCyclicShifts(input, a, b));
 
-        for (int i = 0; i < inputLength; i++)
+        int endPosition = Array.IndexOf(rotations, 0);
+        var result = new char[inputLength];
+        for (var i = 0; i < inputLength; i++)
         {
-            lastCharacters[i] = rotations[i][inputLength - 1];
+            result[i] = input[(rotations[i] + inputLength - 1) % inputLength];
         }
 
-        string result = new string(lastCharacters);
-        return (result, position);
+        return (new string(result), endPosition);
     }
 
+    /// <summary>
+    /// Comparisons of two cyclic shifts.
+    /// </summary>
+    /// <param name="input">Input string.</param>
+    /// <param name="indexA">The index of the first character of the first cyclic shift.</param>
+    /// <param name="indexB">The index of the first character of the second cyclic shift.</param>
+    /// <returns>Comparison result.</returns>
+    private static int CompareCyclicShifts(string input, int indexA, int indexB)
+    {
+        int length = input.Length;
+
+        for (var i = 0; i < length; i++)
+        {
+            char charA = input[(indexA + i) % length];
+            char charB = input[(indexB + i) % length];
+
+            if (charA != charB)
+            {
+                return charA.CompareTo(charB);
+            }
+        }
+
+        return 0;
+    }
+
+    /// <summary>
+    /// Reverse Burrows-Wheeler transformation.
+    /// </summary>
+    /// <param name="transformedString">transformable string.</param>
+    /// <param name="position">end of string position.</param>
+    /// <returns>the string before the BWT transformation.</returns>
     private static string ReverseBwt(string transformedString, int position)
     {
         int length = transformedString.Length;
         char[] firstColumn = transformedString.ToCharArray();
         Array.Sort(firstColumn);
 
-        int[] nextIndex = new int[length];
-        bool[] usedPositions = new bool[length];
+        var nextIndex = new int[length];
+        var usedPositions = new bool[length];
 
-        for (int i = 0; i < length; i++)
+        for (var i = 0; i < length; i++)
         {
-            for (int j = 0; j < length; j++)
+            for (var j = 0; j < length; j++)
             {
                 if (transformedString[j] == firstColumn[i] && !usedPositions[j])
                 {
@@ -71,9 +113,9 @@
             }
         }
 
-        char[] result = new char[length - 1];
+        var result = new char[length];
         int currentPosition = position;
-        for (int i = 0; i < length - 1; i++)
+        for (var i = 0; i < length; i++)
         {
             result[i] = firstColumn[currentPosition];
             currentPosition = nextIndex[currentPosition];
@@ -82,26 +124,33 @@
         return new string(result);
     }
 
+    /// <summary>
+    /// Tests for BWT.
+    /// </summary>
+    /// <returns>True if the tests were passed and false if not.</returns>
     private static bool RunAllTests()
     {
-        string inputTest1 = "a";
+        const string inputTest1 = "a";
         (string outputTest1, int testPosition1) = Bwt(inputTest1);
-        if (inputTest1 != ReverseBwt(outputTest1, testPosition1))
+        if (ReverseBwt(outputTest1, testPosition1) != inputTest1)
         {
+            Console.WriteLine("test 1");
             return false;
         }
 
-        string inputTest2 = "banana";
+        const string inputTest2 = "banana";
         (string outputTest2, int testPosition2) = Bwt(inputTest2);
-        if (inputTest2 != ReverseBwt(outputTest2, testPosition2))
+        if (ReverseBwt(outputTest2, testPosition2) != inputTest2)
         {
+            Console.WriteLine("test 2");
             return false;
         }
 
-        string inputTest3 = "mississippi";
+        const string inputTest3 = "mississippi";
         (string outputTest3, int testPosition3) = Bwt(inputTest3);
-        if (inputTest3 != ReverseBwt(outputTest3, testPosition3))
+        if (ReverseBwt(outputTest3, testPosition3) != inputTest3)
         {
+            Console.WriteLine("test 3");
             return false;
         }
 
