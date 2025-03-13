ae032937c01580cfdd3117594d77e0dcfdd60449
quepum <118292850+quepum@users.noreply.github.com>
2025-03-13 23:07:53 +0300

    feat: Add a Trie adapted to the LZW algorithm.

---
+++ AlgorithmLZW/AlgorithmLZW/Program.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using AlgorithmLZW;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Ошибка: не указан путь к файлу");
+    return;
+}
+
+string filePath = args[0];
+string key = args[1];
+try
+{
+    if (!File.Exists(filePath))
+    {
+        Console.WriteLine("Файл не найден");
+        return;
+    }
+
+    switch (key)
+    {
+        case "-c":
+            LzwCompression.Compress(filePath);
+            Console.WriteLine("Successful compression");
+            break;
+        case "-u":
+            LzwDecompression.Decompress(filePath);
+            Console.WriteLine("Successful decompression");
+            break;
+        default:
+            throw new ArgumentException($"Unknown parameter: {key}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
+}
---
+++ AlgorithmLZW/AlgorithmLZW/Trie.cs
@@ -0,0 +1,76 @@
+﻿namespace AlgorithmLZW;
+
+public class Trie
+{
+    private readonly TrieNode root = new();
+
+    public Trie InitializeByteCodes()
+    {
+        Trie trie = new();
+        for (int i = 0; i < 256; i++)
+        {
+            trie.Add([(byte)i], i);
+        }
+
+        return trie;
+    }
+
+    // Добавление последовательности в дерево
+    public void Add(IEnumerable<byte> sequence, int code)
+    {
+        var node = this.root;
+        foreach (byte item in sequence)
+        {
+            if (!node.Children.TryGetValue(item, out var child))
+            {
+                child = new TrieNode();
+                node.Children[item] = child;
+            }
+
+            node = child;
+        }
+
+        node.Code = code;
+    }
+
+    // Проверка наличия последовательности в дереве
+    public bool Contains(IEnumerable<byte> sequence)
+    {
+        var node = this.root;
+        foreach (byte item in sequence)
+        {
+            if (!node.Children.TryGetValue(item, out var child))
+            {
+                return false;
+            }
+
+            node = child;
+        }
+
+        return node.Code.HasValue;
+    }
+
+    // Получение кода для последовательности
+    public int? GetCode(IEnumerable<byte> sequence)
+    {
+        var node = this.root;
+        foreach (byte item in sequence)
+        {
+            if (!node.Children.TryGetValue(item, out var child))
+            {
+                return null;
+            }
+
+            node = child;
+        }
+
+        return node.Code;
+    }
+
+    private class TrieNode
+    {
+        public Dictionary<byte, TrieNode> Children { get; } = new();
+
+        public int? Code { get; set; }
+    }
+}
