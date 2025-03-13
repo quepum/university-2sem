c8a498d7e5b20fa3d72912866e8a7f87996983c3
quepum <118292850+quepum@users.noreply.github.com>
2025-03-13 23:09:52 +0300

    feat: Add a compression function.

---
+++ AlgorithmLZW/AlgorithmLZW/LzwCompressor.cs
@@ -0,0 +1,51 @@
+﻿namespace AlgorithmLZW;
+
+public static class LzwCompression
+{
+    public static void Compress(string inputFilePath)
+    {
+        byte[] inputData = File.ReadAllBytes(inputFilePath);
+
+        var trie = new Trie().InitializeByteCodes();
+        List<int> compressed = [];
+
+        int startCode = 256;
+        List<byte> current = [];
+
+        foreach (byte item in inputData)
+        {
+            var next = new List<byte>(current) { item };
+
+            if (trie.Contains(next))
+            {
+                current = next;
+            }
+            else
+            {
+                int currentCode = trie.GetCode(current).Value;
+                compressed.Add(currentCode);
+
+                trie.Add(next, startCode);
+                startCode++;
+                current = [item];
+            }
+        }
+
+        if (current.Count > 0)
+        {
+            int currentCode = trie.GetCode(current).Value;
+            compressed.Add(currentCode);
+        }
+
+        string outputFilePath = Path.ChangeExtension(inputFilePath, ".zipped");
+        using (var writer = new BinaryWriter(File.Open(outputFilePath, FileMode.Create)))
+        {
+            foreach (int code in compressed)
+            {
+                writer.Write(code);
+            }
+        }
+
+        Console.WriteLine($"Compressed file saved as: {outputFilePath}");
+    }
+}
