996c3638cab5e47bc344ddf449ab377f20e8e38e
quepum <118292850+quepum@users.noreply.github.com>
2025-03-25 22:34:45 +0300

    feat: add the NetworkNotConnectedException exception class to handle network connectivity errors.

---
+++ Routers/Routers/NetworkNotConnectedException.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="NetworkNotConnectedException.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Routers;
+
+/// <summary>
+/// This exception is used when a router network connectivity issue is detected.
+/// Indicates problems with the network topology, in which routers cannot efficiently transfer data between themselves.
+/// </summary>
+public class NetworkNotConnectedException() : Exception("The network is not connected.");
