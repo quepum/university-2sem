a894b5e8c905db4b60868118c7e6c4c44035eeba
quepum <118292850+quepum@users.noreply.github.com>
2025-03-27 22:55:02 +0300

    style: fix a typo.

--- Routers/Routers/NetworkDisconnectedException.cs
+++ Routers/Routers/NetworkDisconnectedException.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="NetworkNotConnectedException.cs" company="PlaceholderCompany">
+﻿// <copyright file="NetworkDisconnectedException.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
