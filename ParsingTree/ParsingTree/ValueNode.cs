6f72ce023dc2eba6adc337305b7e29b569f3b90d
quepum <118292850+quepum@users.noreply.github.com>
2025-04-04 23:27:21 +0300

    feat: add ValueNode class.

---
+++ ParsingTree/ParsingTree/ValueNode.cs
@@ -0,0 +1,14 @@
+﻿namespace ParsingTree;
+
+public class ValueNode(int value) : Node
+{
+    public override int Evaluate()
+    {
+        return value;
+    }
+
+    public override string Print()
+    {
+        return value.ToString();
+    }
+}
