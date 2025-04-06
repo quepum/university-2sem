b92614343a88589ff9026ed3038e9f0d263f32db
quepum <118292850+quepum@users.noreply.github.com>
2025-04-06 22:59:56 +0300

    docs: add XML comments.

--- ParsingTree/ParsingTree/ValueNode.cs
+++ ParsingTree/ParsingTree/ValueNode.cs
@@ -1,12 +1,27 @@
-﻿namespace ParsingTree;
+﻿// <copyright file="ValueNode.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
+namespace ParsingTree;
+
+/// <summary>
+/// This class represents a numeric operand.
+/// </summary>
 public class ValueNode(int value) : Node
 {
+    /// <summary>
+    /// Returns the value of a number.
+    /// </summary>
+    /// <returns>The numeric value of the node.</returns>
     public override int Evaluate()
     {
         return value;
     }
 
+    /// <summary>
+    /// Converting a number to a string.
+    /// </summary>
+    /// <returns>The numeric value of the node in string format.</returns>
     public override string Print()
     {
         return value.ToString();
