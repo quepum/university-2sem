365b634132295db4dbf9935e0ccc102f59d4289a
quepum <118292850+quepum@users.noreply.github.com>
2025-04-06 23:05:36 +0300

    docs: add XML comments.

--- ParsingTree/ParsingTree/OperatorNode.cs
+++ ParsingTree/ParsingTree/OperatorNode.cs
@@ -1,50 +1,69 @@
-﻿namespace ParsingTree;
+﻿// <copyright file="OperatorNode.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-public class OperatorNode : Node
+namespace ParsingTree
 {
-    private readonly char operation;
-    private readonly Node left;
-    private readonly Node right;
-
-    public OperatorNode(char operation, Node left, Node right)
+    /// <summary>
+    /// Represents an operator node in the expression parse tree.
+    /// </summary>
+    public class OperatorNode : Node
     {
-        if (!"/*-+/".Contains(this.operation))
+        private readonly char operation;
+        private readonly Node left;
+        private readonly Node right;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OperatorNode"/> class.
+        /// </summary>
+        /// <param name="operation">The arithmetic operator ('+', '-', '*', '/').</param>
+        /// <param name="left">The left operand of the operator.</param>
+        /// <param name="right">The right operand of the operator.</param>
+        public OperatorNode(char operation, Node left, Node right)
         {
-            throw new Exception(); // написать своё исключение или подумать какое подходит
+            if (!"+-*/".Contains(operation))
+            {
+                throw new ArgumentException($"Unknown operation: {operation}");
+            }
+
+            this.operation = operation;
+            this.left = left;
+            this.right = right;
         }
 
-        this.operation = operation;
-        this.left = left;
-        this.right = right;
-    }
-
-    public override int Evaluate()
-    {
-        int leftElement = left.Evaluate();
-        int rightElement = right.Evaluate();
-
-        switch (operation)
+        /// <summary>
+        /// Evaluates the value of the operator node by recursively evaluating its left and right operands.
+        /// </summary>
+        /// <returns>The result of applying the operator to the evaluated operands.</returns>
+        public override int Evaluate()
         {
-            case '/':
-                if (rightElement == 0)
-                {
-                    throw new Exception(); // ошибка деление на 0
-                }
+            int leftValue = this.left.Evaluate();
+            int rightValue = this.right.Evaluate();
 
-                return leftElement / rightElement;
-            case '*':
-                return leftElement * rightElement;
-            case '-':
-                return leftElement - rightElement;
-            case '+':
-                return leftElement + rightElement;
-            default:
-                throw new Exception(); // неизвестная операция
+            switch (this.operation)
+            {
+                case '+': return leftValue + rightValue;
+                case '-': return leftValue - rightValue;
+                case '*': return leftValue * rightValue;
+                case '/':
+                    if (rightValue == 0)
+                    {
+                        throw new DivideByZeroException("Division by zero");
+                    }
+
+                    return leftValue / rightValue;
+                default:
+                    throw new InvalidOperationException("Incorrect operation");
+            }
         }
-    }
 
-    public override string Print()
-    {
-        return $"( {operation} {left.Print()} {right.Print()} )";
+        /// <summary>
+        /// Returns a string representation of the operator node in prefix notation.
+        /// </summary>
+        /// <returns>A string in the format "(operator left right)".</returns>
+        public override string Print()
+        {
+            return $"({this.operation} {this.left.Print()} {this.right.Print()})";
+        }
     }
 }
