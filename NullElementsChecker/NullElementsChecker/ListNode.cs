2a34d1f3efd63dbd49b9d4c6bc016f05ed83b437
quepum <118292850+quepum@users.noreply.github.com>
2025-05-23 15:17:56 +0300

    feat: add solution.

---
+++ NullElementsChecker/NullElementsChecker/ListNode.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="MyLinq.cs" author="Alina Letyagina">
+// under MIT License.
+// </copyright>
+
+namespace NullElementsChecker;
+
+/// <summary>
+/// Represents a node in a linked list.
+/// </summary>
+/// <typeparam name="T">The type of data stored in the node.</typeparam>
+/// <param name="data">The data to be stored in the node.</param>
+public class ListNode<T>(T data)
+{
+    /// <summary>
+    /// Gets or sets the data contained in the node.
+    /// </summary>
+    public T Data { get; set; } = data;
+
+    /// <summary>
+    /// Gets or sets the reference to the next node in the linked list.
+    /// </summary>
+    public ListNode<T>? Next { get; set; }
+}
---
+++ NullElementsChecker/NullElementsChecker/StrNullChecker.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="MyLinq.cs" author="Alina Letyagina">
+// under MIT License.
+// </copyright>
+
+namespace NullElementsChecker;
+
+/// <summary>
+/// Implementation of <see cref="INullElementsChecker"/> that specifically checks if a string object is null or empty.
+/// </summary>
+public class StrNullChecker : INullElementsChecker
+{
+    /// <summary>
+    /// Determines whether the specified object is a null or empty string.
+    /// </summary>
+    /// <param name="obj">The object to check. Expected to be of type <see cref="string"/>.</param>
+    /// <returns>
+    /// <c>true</c> if the object is a null reference or an empty string
+    /// <c>false</c> if the object is a non-null, non-empty string or of a different type.
+    /// </returns>
+    public bool IsNull(object obj)
+    {
+        return obj is string s && string.IsNullOrEmpty(s);
+    }
+}
